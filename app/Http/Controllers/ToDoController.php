7a8382493cd6e803d4661376ebe147b9269e1e61
Eren Özdemir <ozdemreren60@gmail.com>
2025-06-04 00:54:43 +0300

    ToDoController prototype added

---
+++ app/Http/Controllers/ToDoController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ToDoController extends Controller
+{
+    public function home(){
+
+    } 
+    public function store(){
+
+    }
+    public function edit(){
+
+    }
+    public function delete(){
+        
+    }
+
+}
